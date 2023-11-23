1b382cb247210593165a8e51b72d6c3bd0d1b499
zaheerahmed8845 <zaheerahmed8845@gmail.com>
2023-11-23 21:59:17 +0530

    JDK Initial Commit

---
+++ jdk/jdk10/optional-or-else-throw/src/main/java/com/example/orelsethrow/OptionalOrElseThrow.java
@@ -0,0 +1,48 @@
+package com.example.orelsethrow;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+//Example of orElseThrow method in Optional class.
+public class OptionalOrElseThrow {
+
+    public static void main(String[] args) {
+
+        OptionalOrElseThrow optionalOrElse = new OptionalOrElseThrow();
+        try {
+            String largeWord = optionalOrElse.findLargeWord();
+            System.out.println("Large word is : " + largeWord);
+        } catch (NoSuchElementException ex) {
+            System.out.println("No such element found exception");
+        }
+        String largeWordUsingOrElseThrowMethod = optionalOrElse.findLargeWordUsingOrElseThrowMethod();
+        System.out.println("Large word using or else throw method : " + largeWordUsingOrElseThrowMethod);
+    }
+
+    public String findLargeWord() {
+
+        Optional<String> largeWord = searchLargeWord();
+        //Avoid
+        if (largeWord.isPresent()) {
+            return largeWord.get();
+        }
+        throw new NoSuchElementException("Word not found...");
+    }
+
+    public String findLargeWordUsingOrElseThrowMethod() {
+
+        Optional<String> largeWord = searchLargeWord();
+        //Right way
+        return largeWord.orElseThrow();
+
+    }
+
+    private Optional<String> searchLargeWord() {
+
+        return Stream.of("football", "12345678", "hellojava")
+                .filter(s -> s.length() >= 20)
+                .findAny();
+    }
+
+}
