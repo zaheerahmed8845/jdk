182bfe7ab330af2732c03b109dfc3858f1fdfa8c
zaheerahmed8845 <zaheerahmed8845@gmail.com>
2023-12-18 22:12:01 +0530

    JDK Initial Commit

---
+++ jdk/jdk14/pattern-matching-instanceof-preview/src/main/java/com/example/patternmatching/InstanceOf.java
@@ -0,0 +1,24 @@
+package com.example.patternmatching;
+
+import java.util.Date;
+
+public class InstanceOf {
+
+    public static void main(String[] args) {
+
+        Object object = someThirdPartyCode();
+
+        if (object instanceof String result && result.length() > 5) {
+            System.out.println(result.toUpperCase());
+        } else if (object instanceof Date result) {
+            System.out.println(result.getTime());
+        } else if (object instanceof Number result) {
+            System.out.println(result.intValue());
+        }
+    }
+
+    private static Object someThirdPartyCode() {
+        return 10;
+    }
+
+}
